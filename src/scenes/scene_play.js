f1dd6920d8bcf86a48de0c04d9a79646830f3693
Sibona <javier.sibona@icbc.com.ar>
2019-10-08 17:57:37 -0300

    Add new text

--- src/scenes/scene_play.js
+++ src/scenes/scene_play.js
@@ -1,12 +1,22 @@
 import Palas from '../gameObjects/palas.js';
-class Scene_play extends Phaser.Scene {
-    constructor() {
+class Scene_play extends Phaser.Scene 
+{
+    constructor() 
+    {
         super({key: "Scene_play"});
     }
 
-    create() {
+    create()
+    {
         let center_width = this.sys.game.config.width/2;
         let center_height = this.sys.game.config.height/2;
+
+        // Score
+        this.score_izq = 0;
+        this.score_der = 0;
+        var style = { font: "bold 32px Arial", fill: "#fff", align: "center", boundsAlignH: "center", boundsAlignV: "middle" };
+        this.scoreText_izq = this.add.text(center_width - 48, 16, '0', style);
+        this.scoreText_der = this.add.text(center_width + 28, 16, '0', style);
 
         // Separador
         this.add.image(center_width, center_height, "separador");
@@ -34,33 +44,53 @@ class Scene_play extends Phaser.Scene {
         this.cursor_W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         this.cursor_S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
     }
-    update() {
-        if(this.ball.x < 0 || this.ball.x > this.sys.game.config.width) {
+
+    update()
+    {
+        if(this.ball.x < 0)
+        {
+            this.score_der++;
+            this.scoreText_der.setText(this.score_der);
+            this.ball.setPosition(this.sys.game.config.width/2, this.sys.game.config.height/2);
+        }
+        else if(this.ball.x > this.sys.game.config.width)
+        {
+            this.score_izq++;
+            this.scoreText_izq.setText(this.score_izq);
             this.ball.setPosition(this.sys.game.config.width/2, this.sys.game.config.height/2);
         }
 
         // Control de las palas
         // pala derecha
-        if(this.cursor.down.isDown) {
+        if(this.cursor.down.isDown)
+        {
             this.derecha.body.setVelocityY(300);
-        } else if(this.cursor.up.isDown) {
+        }
+        else if(this.cursor.up.isDown)
+        {
             this.derecha.body.setVelocityY(-300);
-        } else {
+        }
+        else
+        {
             this.derecha.body.setVelocityY(0);
         }
         // Pala izquierda
-        if(this.cursor_S.isDown) {
+        if(this.cursor_S.isDown)
+        {
             this.izquierda.body.setVelocityY(300);
-        } else if(this.cursor_W.isDown) {
+        }
+        else if(this.cursor_W.isDown)
+        {
             this.izquierda.body.setVelocityY(-300);
-        } else {
+        }
+        else
+        {
             this.izquierda.body.setVelocityY(0);
         }
-
-
     }
 
-    chocaPala() {
+    chocaPala()
+    {
         this.ball.setVelocityY(Phaser.Math.Between(-120, 120));
     }
 }
